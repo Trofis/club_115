99731dfbbe56b8b734c1e317f70aa2cf140d2bf7
Trofis <porto.thomas@gmail.com>
2021-12-03 16:46:45 +0100

    Img resized function done - generate kick off in progress

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import json
 from PIL import Image, ImageFont, ImageDraw, ImageChops
 import requests
 from datetime import datetime
+import os
+from enum import Enum
 
 f = open('data_clubs.json')
 data_clubs = json.load(f)
 
+class Ligues(Enum):
+    GERMANY = 1,
+    SPAIN = 2,
+    ENGLAND = 3,
+    ITALY = 4,
+    FRANCE = 5
 
 
 ################# FUNCTIONS ####################
@@ -19,6 +27,7 @@ def trim(im):
     bbox = diff.getbbox()
     if bbox:
         return im.crop(bbox)
+    return im
 
 # Replace transparent to white background
 # param : im (obj Image)
@@ -38,51 +47,97 @@ def transparent(img):
 
     return rgba
 
-def generate_img(clubs):
+# Generate correct images
+# param : path_clubs_images (String)
+#         ligue (Enum Ligues)
+def generate_img(ligue):
     ratio = 0.35
+    print(os.listdir(get_path_img_per_ligue(ligue)))
+    for filename in os.listdir(get_path_img_per_ligue(ligue)):
+        print(filename)
+        img = Image.open(get_path_img_per_ligue(ligue)+"/"+filename)
+        width, height = img.size
+        print(img)
+        rgba = transparent(img)
+        print(rgba)
 
-    for club in clubs:
-        img = Image.open(data_clubs['path_logo_bundesliga']+"/mainz_2.png")
-        width, height = im1.size
+        rgba = trim(rgba.resize((int(width*ratio),int(height*ratio)), Image.ANTIALIAS))
+        print(rgba)
+
+        rgba.save(get_path_img_resized_per_ligue(ligue)+"/"+filename)
+
+
+# Get league img logo path 
+# param : ligue (Enum Ligues)
+#         
+def get_path_img_per_ligue(ligue):
+    if ligue.GERMANY:
+        return  data_clubs['path_logo_bundesliga'] 
+    elif ligue.SPAIN:
+        return  data_clubs['path_logo_seria_a'] 
+    elif ligue.ENGLAND:
+        return  data_clubs['path_logo_premier_league'] 
+    elif ligue.ITALY:
+        return  data_clubs['path_logo_seria_a'] 
+    return data_clubs['path_logo_ligue_1'] 
+
+# Get league img logo resized path 
+# param : ligue (Enum Ligues)
+#       
+def get_path_img_resized_per_ligue(ligue):
+    if ligue.GERMANY:
+        return  data_clubs['path_logo_resized_bundesliga'] 
+    elif ligue.SPAIN:
+        return  data_clubs['path_logo_resized_seria_a'] 
+    elif ligue.ENGLAND:
+        return  data_clubs['path_logo_resized_premier_league'] 
+    elif ligue.ITALY:
+        return  data_clubs['path_logo_resized_seria_a'] 
+    return data_clubs['path_logo_resized_ligue_1'] 
 
 
     
-    rgba = trim(rgba.resize((int(width*ratio),int(height*ratio)), Image.ANTIALIAS))
-    rgba.save(data_clubs['path_logo_resized']+)
+    
 
 ################## BUNDESLIGA TEST ####################
-# LOAD IMAGE & COPY
-template = Image.open(data_clubs['path_templates']+"/calendrier_bundesliga.png")
-new_image = template.copy()
+# Generate next kick off image
+# param : ligue (Enum Ligues)
+#       
+def generate_next_kick_off(ligue):
+    # LOAD IMAGE & COPY
+    template = Image.open(data_clubs['path_templates']+"/calendrier_bundesliga.png")
+    new_image = template.copy()
 
-#FONT
-league_gothic = ImageFont.truetype(data_clubs['path_font']+'/league_gothic/LeagueGothic-Regular.otf', 30, layout_engine=ImageFont.LAYOUT_RAQM)
-ruda_bold_teams = ImageFont.truetype(data_clubs['path_font']+'/ruda/Ruda-Bold.ttf', 20, layout_engine=ImageFont.LAYOUT_RAQM)
-ruda_bold_time = ImageFont.truetype(data_clubs['path_font']+'/ruda/Ruda-Bold.ttf', 25, layout_engine=ImageFont.LAYOUT_RAQM)
+    # FONT
+    league_gothic = ImageFont.truetype(data_clubs['path_font']+'/league_gothic/LeagueGothic-Regular.otf', 30, layout_engine=ImageFont.LAYOUT_RAQM)
+    ruda_bold_teams = ImageFont.truetype(data_clubs['path_font']+'/ruda/Ruda-Bold.ttf', 20, layout_engine=ImageFont.LAYOUT_RAQM)
+    ruda_bold_time = ImageFont.truetype(data_clubs['path_font']+'/ruda/Ruda-Bold.ttf', 25, layout_engine=ImageFont.LAYOUT_RAQM)
 
-#DRAW IMAGE
-edit_image = ImageDraw.Draw(new_image)
+    # DRAW ON TEMPLATE
+    edit_image = ImageDraw.Draw(new_image)
 
-f = open('test.json')
-germany = json.load(f)['germany']
-
-
-start_height = 260
-for play in germany:
-    new_image.paste(rgba, (145,start_height-15) )
-    new_image.paste(rgba, (890,start_height-15) )
-
-    date_time = datetime.fromisoformat(play[1]) 
-    edit_image.text((35,start_height),"{}".format(str(date_time.day)+"/"+str(date_time.month)).upper(), (0,0,0), ruda_bold_time )
-    edit_image.text((998,start_height),"{}".format(str(date_time.hour)+":"+str(date_time.minute)).upper(), (0,0,0), ruda_bold_time )
-    edit_image.text((220,start_height), play[3][1].upper(), (255,255,255), ruda_bold_teams)
-    size = ruda_bold_teams.getsize(play[4][1])
-    edit_image.text((872-size[0],start_height), play[4][1], (255,255,255), ruda_bold_teams)
-    start_height += 85
+    # LOAD DATA
+    f = open('test.json')
+    germany = json.load(f)['germany']
 
 
+    start_height = 260
+    for play in germany:
+        new_image.paste(rgba, (145,start_height-15) )
+        new_image.paste(rgba, (890,start_height-15) )
 
-#SAVE IMAGE
-new_image.save(data_clubs['path_render']+"/bundesliga.png", quality=100)
+        date_time = datetime.fromisoformat(play[1]) 
+        edit_image.text((35,start_height),"{}".format(str(date_time.day)+"/"+str(date_time.month)).upper(), (0,0,0), ruda_bold_time )
+        edit_image.text((998,start_height),"{}".format(str(date_time.hour)+":"+str(date_time.minute)).upper(), (0,0,0), ruda_bold_time )
+        edit_image.text((220,start_height), play[3][1].upper(), (255,255,255), ruda_bold_teams)
+        size = ruda_bold_teams.getsize(play[4][1])
+        edit_image.text((872-size[0],start_height), play[4][1], (255,255,255), ruda_bold_teams)
+        start_height += 85
 
 
+
+    #SAVE IMAGE
+    new_image.save(data_clubs['path_render']+"/bundesliga.png", quality=100)
+
+
+generate_img(Ligues.GERMANY)
